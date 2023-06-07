ec89e614980faee1dc269be07b154459aa617178
anirudh <anirudh04mishra@gmail.com>
2023-06-07 16:00:22 +0530

    feat: init graph

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import CareerGraph from '@/components/CareerGraph';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-3xl mb-10">The Next Chapter</h1>
+      <CareerGraph />
+    </main>
+  );
+}
---
+++ frontend/src/components/CareerGraph.js
@@ -0,0 +1,44 @@
+'use client';
+import React, { useState } from 'react';
+import ReactFlow from 'reactflow';
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Input Node' },
+    position: { x: 250, y: 25 },
+  },
+
+  {
+    id: '2',
+    // you can also pass a React component as a label
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: '3',
+    type: 'output',
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 },
+  },
+];
+
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e2-3', source: '2', target: '3', animated: true },
+];
+
+function CareerGraph({ data }) {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  return (
+    <div className="h-32 w-32">
+      <ReactFlow nodes={nodes} edges={edges} fitView />
+    </div>
+  );
+}
+
+export default CareerGraph;
