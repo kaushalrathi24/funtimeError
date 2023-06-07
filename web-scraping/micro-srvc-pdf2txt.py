04d329f19a6818196613f01bef6f1d3fb72eb1be
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-06-07 18:26:57 +0530

    feat: added a flask microservice for pdf to text conversion - untested
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

---
+++ web-scraping/micro-srvc-pdf2txt.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Pass the required route to the decorator.
+@app.route("/converter/<string:filename>")
+def converter(filename):
+    # importing required modules
+    from pypdf import PdfReader
+
+    file = rf"/home/<username>/{filename}.pdf"
+    # creating a pdf reader object
+    reader = PdfReader(file)
+
+    # printing number of pages in pdf file
+    print(len(reader.pages))
+
+    text = ""
+
+    for i in range(len(reader.pages)):
+        # getting a specific page from the pdf file
+        page = reader.pages[0]
+        # extracting text from page
+        text += page.extract_text()
+
+    # print(text)
+    return text
+	
+if __name__ == "__main__":
+	app.run(debug=True)
+
